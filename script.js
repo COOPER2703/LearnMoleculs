18bd6469b1efa7583207346942c634186de2e7f8
cooper <jolan.cochet@gmail.com>
2021-11-11 14:28:22 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var Lipides = new Array("Acide alpha-linoléique", "Acide arachidonique", "Acide linoléique", "Acide oléique", "Acide palmitique", "Acide stéarique", "Cholestérol", "Glycérol", "GM1 ganglioside", "Isoprene", "Phosphatidylcholine", "Phosphatidylethanolamine", "Phosphatidylglycerol", "Phosphatidylinositol", "Phosphatidylserine", "Sphingosine");
 var Glucides = new Array("Saccharose", "Lactose", "Cellobiose", "Maltose", "Acide pyruvique", "Dihydroxyacetone", "Mannose", "N-acetyl-D-glucosamine", "NANA", "Beta-D-mannopyranose", "Acide beta-D-glucuronique", "Glucose", "B-D-glucosamine", "D-glyceraldehyde", "Galactose", "D-fructose", "Alpha-D-glucopyranose", "L-fructose", "Ribose", "2-desoxy-D-ribofuranose", "Alpha-D-fructofuranose", "Beta-D-Ribofuranose", "Beta-D-galactopyranose")
 var Acides_amines = new Array("Serotonine", "Beta-alanine", "Histamine", "Glycine", "Histidine", "Valine", "Alanine", "Leucine", "Isoleucine", "Methionine", "Glutamate", "Aspartate", "Phenylalanine", "Glutamine", "Asparagine", "Serine", "Tyrosine", "Threonine", "Proline", "GABA", "Cysteine", "Lysine", "Arginine", "Tryptophane")
@@ -52,17 +51,16 @@ function chooseGoodAnswer() {
 
 }
 
+var delayInMilliseconds = 1000; //1 second
+
 
 function verificate(rep) {
 
     if (rep === goodanswer) {
+        document.getElementById("goodorfalse").innerHTML = "Bonne réponse" 
 
-        //Mettre l'animation de bonne réponse ici
-
-    } else {
-
-       //mettre l'animation de mauvaise réponse ici
-
+    } else {       
+        document.getElementById("goodorfalse").innerHTML = "Mauvaise réponse"
     }
 
 }
@@ -75,12 +73,17 @@ function changeAnswers(rep, condition) {
         verificate(rep)
     }
     
-    listAnswers(using_tab)
-    chooseGoodAnswer()
+    setTimeout(function() {
+        listAnswers(using_tab)
+        chooseGoodAnswer()
 
-    document.getElementById("rep1").innerHTML= answers[0];
-    document.getElementById("rep2").innerHTML= answers[1];
-    document.getElementById("rep3").innerHTML= answers[2];
-    document.getElementById("rep4").innerHTML= answers[3];
+        document.getElementById("rep1").innerHTML= answers[0];
+        document.getElementById("rep2").innerHTML= answers[1];
+        document.getElementById("rep3").innerHTML= answers[2];
+        document.getElementById("rep4").innerHTML= answers[3];
+        document.getElementById("goodorfalse").innerHTML = "" 
+    }, delayInMilliseconds);
+
+
 
 }
