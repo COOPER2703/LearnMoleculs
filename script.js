f15ba69d2aea5de6a828fd647cd310d20b41f037
cooper <jolan.cochet@gmail.com>
2021-11-11 11:36:20 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 
 var Lipides = new Array("Acide alpha-linoléique", "Acide arachidonique", "Acide linoléique", "Acide oléique", "Acide palmitique", "Acide stéarique", "Cholestérol", "Glycérol", "GM1 ganglioside", "Isoprene", "Phosphatidylcholine", "Phosphatidylethanolamine", "Phosphatidylglycerol", "Phosphatidylinositol", "Phosphatidylserine", "Sphingosine");
-var Glucides = new Array("Saccharose", "Lactose", "Cellobiose", "Maltose", "Acide pyruvique", "Dihydroxyacetone", "Mannose", "N-acetyle-D-glucosamine", "NANA", "Beta-D-mannopyranose", "Acide beta-D-glucuronique", "Glucose", "B-D-glucosamine", "D-glyceradldehyde", "Galactose", "D-fructose", "Alpha-D-glucopyranose", "L-fructose", "Ribose", "2-Desoxy-D-ribofuranose", "Alpha-D-fructofuranose", "Beta-D-ribofuranose", "Beta-D-galactopyranose");
-var Acide_amine = new Array("Serotonine", "Beta-alanine", "Histamine", "Glycine", "Histidine", "Valine", "Alanine", "Leucine", "Isoleucine", "Methionine", "Glutamate", "Asparate", "Phenylalanine", "Glutamine", "Asparagine", "Serine", "Tyrosine", "Threonine", "Proline", "GABA", "Cysteine", "Lysine", "Arginine", "Tryptophane");
+var Glucides = new Array("Saccharose", "Lactose", "Cellobiose", "Maltose", "Acide pyruvique", "Dihydroxyacetone", "Mannose", "N-acetyl-D-glucosamine", "NANA", "Beta-D-mannopyranose", "Acide beta-D-glucuronique", "Glucose", "B-D-glucosamine", "D-glyceraldehyde", "Galactose", "D-fructose", "Alpha-D-glucopyranose", "L-fructose", "Ribose", "2-desoxy-D-ribofuranose", "Alpha-D-fructofuranose", "Beta-D-Ribofuranose", "Beta-D-galactopyranose")
+var Acides_amines = new Array("Serotonine", "Beta-alanine", "Histamine", "Glycine", "Histidine", "Valine", "Alanine", "Leucine", "Isoleucine", "Methionine", "Glutamate", "Aspartate", "Phenylalanine", "Glutamine", "Asparagine", "Serine", "Tyrosine", "Threonine", "Proline", "GABA", "Cysteine", "Lysine", "Arginine", "Tryptophane")
 
 var answers = new Array;
 var goodanswer;
-var switcher = Lipides;
+var using_tab = Lipides;
 
 function getRandomInt(tab) {
     return Math.floor(Math.random() * tab.length);
   }
 
-function listAnswers(tab) {
+function listAnswers(using_tab) {
 
     answers = [];
-    var temp = [...tab]
-
-
-
-    console.log(tab)
+    
+    var temp = [...using_tab]
 
     for (let i = 0; i < 4; i++) {
 
@@ -29,35 +26,29 @@ function listAnswers(tab) {
     
 }
 
+function changeList(list) {
+
+    if (list === 0) {
+        using_tab = Glucides;
+    }
+    if (list === 1) {
+        using_tab = Lipides;
+    }
+    if (list === 2) {
+        using_tab = Acides_amines;
+    }
+    changeAnswers(0, false)
+}
+
 function chooseGoodAnswer() {
 
     goodanswer = getRandomInt(answers)
 
-    document.getElementById("img").src = goodanswer
-
+    document.getElementById("img").innerHTML = answers[goodanswer]
 }
 
-function changeList(list) {
 
-
-    if (list === 0) {
-
-        switcher = Lipides
-    }
-    if (list === 1) {
-
-        switcher = Glucides
-    }
-    if (list === 2) {
-
-        switcher = Acide_amine
-    }
-    changeAnswers()
-
-}
-
-function changeAnswers(rep, ) {
-
+function verificate(rep) {
 
     if (rep === goodanswer) {
 
@@ -66,18 +57,25 @@ function changeAnswers(rep, ) {
     } else {
 
         console.log("Mauvaise réponse")
+
     }
 
+}
 
-    listAnswers(switcher);
-    chooseGoodAnswer();
+function changeAnswers(rep, condition) {
 
-    document.getElementById("img").innerHTML = answers[goodanswer];
-    document.getElementById("rep1").innerHTML = answers[0];
-    document.getElementById("rep2").innerHTML = answers[1];
-    document.getElementById("rep3").innerHTML = answers[2];
-    document.getElementById("rep4").innerHTML = answers[3];
+    console.log(goodanswer)
 
+    if (condition) {
+        verificate(rep)
+    }
+    
+    listAnswers(using_tab)
+    chooseGoodAnswer()
 
+    document.getElementById("rep1").innerHTML= answers[0];
+    document.getElementById("rep2").innerHTML= answers[1];
+    document.getElementById("rep3").innerHTML= answers[2];
+    document.getElementById("rep4").innerHTML= answers[3];
 
 }
