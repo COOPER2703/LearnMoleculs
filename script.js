d02fd498d70d7e2c7503ec7458eab8fd1716cc06
cooper <jolan.cochet@gmail.com>
2021-11-12 10:23:41 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var Lipides = new Array("Acide alpha-linoléique", "Acide arachidonique", "Acide linoléique", "Acide oléique", "Acide palmitique", "Acide stéarique", "Cholestérol", "Glycérol", "GM1 ganglioside", "Isoprene", "Phosphatidylcholine", "Phosphatidylethanolamine", "Phosphatidylglycerol", "Phosphatidylinositol", "Phosphatidylserine", "Sphingosine");
-var Glucides = new Array("Saccharose", "Lactose", "Cellobiose", "Maltose", "Acide pyruvique", "Dihydroxyacetone", "Mannose", "N-acetyl-D-glucosamine", "NANA", "Beta-D-mannopyranose", "Acide beta-D-glucuronique", "Glucose", "B-D-glucosamine", "D-glyceraldehyde", "Galactose", "D-fructose", "Alpha-D-glucopyranose", "L-fructose", "Ribose", "2-desoxy-D-ribofuranose", "Alpha-D-fructofuranose", "Beta-D-Ribofuranose", "Beta-D-galactopyranose")
+var Glucides = new Array("Saccharose", "Lactose", "Cellobiose", "Maltose", "Acide pyruvique", "Dihydroxyacetone", "Mannose", "N-acetyl-D-glucosamine", "NANA", "Beta-D-mannopyranose", "Acide beta-D-glucuronique", "Glucose", "Beta-D-glucosamine", "D-glyceraldehyde", "Galactose", "D-fructose", "Alpha-D-glucopyranose", "L-fructose", "Ribose", "2-desoxy-D-ribofuranose", "Alpha-D-fructofuranose", "Beta-D-Ribofuranose", "Beta-D-galactopyranose")
 var Acides_amines = new Array("Serotonine", "Beta-alanine", "Histamine", "Glycine", "Histidine", "Valine", "Alanine", "Leucine", "Isoleucine", "Methionine", "Glutamate", "Aspartate", "Phenylalanine", "Glutamine", "Asparagine", "Serine", "Tyrosine", "Threonine", "Proline", "GABA", "Cysteine", "Lysine", "Arginine", "Tryptophane")
 
 var answers = new Array;
@@ -56,19 +56,22 @@ function chooseGoodAnswer() {
 
 }
 
-var delayInMilliseconds = 1000; //1 second
+
 
 
 function verificate(rep) {
 
     if (rep === goodanswer) {
-        document.getElementById("goodorfalse").innerHTML = "Bonne réponse" 
+        document.getElementById("rep" + rep.toString()).style.background = "green";
 
     } else {       
-        document.getElementById("goodorfalse").innerHTML = "Mauvaise réponse"
+        document.getElementById("rep" + goodanswer.toString()).style.background = "green";
+        document.getElementById("rep" + rep.toString()).style.background = "red";
     }
 
 }
+
+var delayInMilliseconds = 1000; //1 second
 
 function changeAnswers(rep, condition) {
 
@@ -77,16 +80,22 @@ function changeAnswers(rep, condition) {
     if (condition) {
         verificate(rep)
     }
-    
+
+
     setTimeout(function() {
+
         listAnswers(using_tab)
         chooseGoodAnswer()
 
-        document.getElementById("rep1").innerHTML= answers[0];
-        document.getElementById("rep2").innerHTML= answers[1];
-        document.getElementById("rep3").innerHTML= answers[2];
-        document.getElementById("rep4").innerHTML= answers[3];
-        document.getElementById("goodorfalse").innerHTML = "" 
+        document.getElementById("rep0").innerHTML= answers[0];
+        document.getElementById("rep1").innerHTML= answers[1];
+        document.getElementById("rep2").innerHTML= answers[2];
+        document.getElementById("rep3").innerHTML= answers[3]; 
+
+        for (let i = 0; i < 4; i++) {
+            document.getElementById("rep" + i.toString()).style.background = "#23242999";
+        }
+
     }, delayInMilliseconds);
 
 
